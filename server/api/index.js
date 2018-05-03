95e7b1e8180ab1f6bdd087aaa7323c6726ccc070
Angelo Gazzola <angelo@nglgzz.com>
2018-05-03 16:47:22 +0200

    add endpoint for suggestions, add common words

--- server/api/index.js
+++ server/api/index.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const { logger, APIError } = require('../utils/errors');
+const digitsToWordsMap = require('../data/digitsToWords');
 
 
 const router = express.Router();
 
-// Return a list of word predictions based on a sequence of numbers.
-router.get('/predict', (req, res) => {
+// Return a list of word suggestions based on a sequence of numbers.
+router.get('/suggestions', (req, res) => {
   const { q } = req.query;
 
   if (q.match(/\D/)) {
     throw new APIError(400, 'The query string must contain numbers only');
   }
 
-  res.send({ predictions: [] });
+  res.send({
+    predictions: digitsToWordsMap[q] || [],
+  });
 });
 
 // Handle any error inside the endpoints.
 // eslint-disable-next-line no-unused-vars
 router.use((err, req, res, next) => logger(err, res));
+
 
 module.exports = router;
