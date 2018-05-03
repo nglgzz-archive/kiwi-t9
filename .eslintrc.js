8168666b49e94957d83d96d8506a7b71736607a0
Angelo Gazzola <angelo@nglgzz.com>
2018-05-03 18:05:49 +0200

    add babel parser and webpack resolver for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,23 @@
 module.exports = {
   extends: 'airbnb',
   plugins: ['react', 'jsx-a11y', 'import'],
-  env: { browser: true },
-  rules: { 'react/react-in-jsx-scope': ['off'] },
+
+  env: {
+    browser: true,
+  },
+
+  rules: {
+    'react/react-in-jsx-scope': ['off'],
+  },
+
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+  },
+
+  settings: {
+    'import/resolver': {
+      webpack: { config: 'webpack.config.js' },
+    },
+  },
 };
