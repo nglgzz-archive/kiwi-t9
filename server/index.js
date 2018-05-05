71acef81a99856de59b8f32b27e94f62a16e5957
Angelo Gazzola <angelo@nglgzz.com>
2018-05-05 18:48:18 +0200

    add firebase deploy

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const functions = require('firebase-functions');
 const express = require('express');
 const api = require('./api/index');
 
@@ -7,10 +8,18 @@ const app = express();
 // API
 app.use('/api', api);
 
-// Static files
-app.use('/static', express.static('client/dist'));
-app.use('/static', express.static('client/fonts'));
-app.get('*', (_, res) => res.sendFile('index.html', { root: 'client' }));
+// When running on Firebase static files are not needed as they are hosted by
+// Firebase itself.
+if (!process.env.FIREBASE_CONFIG) {
+  // Static files
+  app.use('/static', express.static('client/dist'));
+  app.use('/static', express.static('client/fonts'));
+  app.get('*', (_, res) => res.sendFile('index.html', { root: 'client' }));
 
-// Magic
-app.listen(3000, () => console.log('Magic started on port 3000!'));
+  // Magic
+  app.listen(3000, () => console.log('Magic started on port 3000!'));
+}
+
+
+// Used to deploy on Firebase.
+exports.app = functions.https.onRequest(app);
